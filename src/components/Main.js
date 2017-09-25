0b36bc70b5b032604f245b750954028aad3fadd7
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-09-25 18:47:31 +0200

    react router change

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ class Main extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path='/' component={About} />
+        <Route exact path='/' render={() => <About/>} />
         <Route path='/projects' component={Projects} />
         <Route path='/contact' component={Contact} />
       </Switch>
